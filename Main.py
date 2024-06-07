09e586f5d6ec0ad73d6c85c869f08f3c2990767f
Devon Browning <browningdevon@gmail.com>
2024-06-06 19:21:39 -0500

    updates

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 import user_actions
+
+
 def main():
 
     while True:
         print('\nLibrary Terminal')
         print('1. Display all books')
         print('2. Search for a book by author')
-        print('3. Search for a book by title keyword')
+        print('3. Search for a book by title')
         print('4. Checkout a book')
         print('5. Return a book')
         print('6. Process returned books')
@@ -24,17 +26,17 @@ def main():
             else:
                 print("No books found by that author.")
         elif choice == '3':
-            keyword = input('Enter the title keyword: ')
-            results = user_actions.search_title(keyword)
+            title = input('Enter the title: ')
+            results = user_actions.search_title(title)
             if results:
                 for book in results:
                     print(book.get_info())
                     print("-" * 40)
             else:
-                print("No books found with that keyword.")
+                print("No books found with that title.")
         elif choice == '4':
-            book_title = input("Enter the title of the book to check out: ")
-            user_actions.checkout_book(book_title)
+            book_title = input("Which book would you like to checkout?: ")
+            user_actions.select_book(book_title)
         elif choice == "5":
             book_title = input("Enter the title of the book to return: ")
             user_actions.return_book(book_title)
@@ -44,6 +46,7 @@ def main():
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
