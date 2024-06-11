217d2c4fcdaf8a8e314da0976893684b99b35f51
Brian Gullen <brian.gullen@gmail.com>
2024-06-10 20:04:42 -0400

    added due date function and attribute

--- media.py
+++ media.py
@@ -1,9 +1,10 @@
 class Books:
-    def __init__(self, title: str, author: str, checked_out: bool, condition: int):
+    def __init__(self, title: str, author: str, checked_out: bool, condition: int, due_date: str):
         self.title = title
         self.author = author
         self.checked_out = checked_out
         self.condition = condition
+        self.due_date = due_date
 
     def update_condition(self):
         self.condition -= 1
@@ -29,18 +30,21 @@ class Books:
         else:
             print('Unreadable')
 
+    def set_due_date(self):
+        return self.due_date == ""
 
-all_books = [Books("1984", "George Orwell", False, 4),
-             Books("The Great Gatsby", "F. Scott Fitzgerald", True, 5),
-             Books("The Hobbit", "J.R.R. Tolkien", False, 2),
-             Books("Brave New World", "Aldous Huxley", False, 3),
-             Books("To Kill a Mockingbird", "Harper Lee", True, 1),
-             Books("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", False, 4),
-             Books("The Martian", "Andy Weir", True, 4),
-             Books("Beloved", "Toni Morrison", False, 5),
-             Books("The Road", "Cormac McCarthy", False, 4),
-             Books("The Name of the Wind", "Patrick Rothfuss", True, 2),
-             Books("The Handmaid's Tale", "Margaret Atwood", False, 3),
-             Books("The Night Circus", "Erin Morgenstern", True, 4),
-             Books("The Picture of Dorian Gray", "Oscar Wilde", False, 1),
-             Books("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", False, 3)]
+
+all_books = [Books("1984", "George Orwell", False, 4, ""),
+             Books("The Great Gatsby", "F. Scott Fitzgerald", True, 5, ""),
+             Books("The Hobbit", "J.R.R. Tolkien", False, 2, ""),
+             Books("Brave New World", "Aldous Huxley", False, 3, ""),
+             Books("To Kill a Mockingbird", "Harper Lee", True, 1, ""),
+             Books("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", False, 4, ""),
+             Books("The Martian", "Andy Weir", True, 4, ""),
+             Books("Beloved", "Toni Morrison", False, 5, ""),
+             Books("The Road", "Cormac McCarthy", False, 4, ""),
+             Books("The Name of the Wind", "Patrick Rothfuss", True, 2, ""),
+             Books("The Handmaid's Tale", "Margaret Atwood", False, 3, ""),
+             Books("The Night Circus", "Erin Morgenstern", True, 4, ""),
+             Books("The Picture of Dorian Gray", "Oscar Wilde", False, 1, ""),
+             Books("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", False, 3, "")]
